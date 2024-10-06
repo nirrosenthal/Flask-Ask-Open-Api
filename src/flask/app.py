721b394fed0e047c4d25cf29c420e8f801e8bba6
ngrvendeta <74234047+nirrosenthal@users.noreply.github.com>
2024-10-06 22:09:04 +0300

    adding ask post request structure

--- src/flask/app.py
+++ src/flask/app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "hello world, from flask"
+@app.route("/ask", methods=["POST"])
+def ask():
+    ask_data = request.get_json()
+    question = ask_data.get("question")
+    response = {
+        question: question,
+        "answer": "defualt answer until openAI feature"
+    }
+    
+    return jsonify(response), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
