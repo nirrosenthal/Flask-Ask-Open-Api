d1dabacd704405e9b522ab04bb46f3cfe475aaf5
ngrvendeta <74234047+nirrosenthal@users.noreply.github.com>
2024-10-07 12:27:52 +0300

    updated test_app for sanity case

--- tests/flask/test_app.py
+++ tests/flask/test_app.py
@@ -1,21 +1,30 @@
 import pytest
 from flask import json
 from src.flask.app import app
-from mockito import when, verify
+import src.flask.app
+from mockito import when, verify, mock
+
 
 @pytest.fixture
-def client():
+def flaskClient():
     app.testing = True
-    with app.test_client() as client:
-        yield client
+    with app.test_client() as flaskClient:
+        yield flaskClient
+
+@pytest.fixture
+def openAIAskQuestion(request):
+    question, answer = request.param    
+    mock_openAI = mock({"question": question, "answer": answer})
+    when(src.flask.app).OpenAIAskQuestion(question).thenReturn(mock_openAI)
+    return mock_openAI
 
 
-def test_ask__sanity_respose__expected_answer_question_response_code_200(client):
-    question = "what do i get from this post request?"
-    data = {"question": question}
-    response = client.post('/ask', json=data)
+@pytest.mark.parametrize('openAIAskQuestion', [("what do i get from this post request?","the question and answer to it")], indirect=True)
+def test_ask__sanity_respose__expected_answer_question_response_code_200(flaskClient, openAIAskQuestion):
+    response = flaskClient.post('/ask', json={"question": openAIAskQuestion.question})
     assert response.status_code == 200
     json_data = response.get_json()
-    assert json_data["question"] == question
-    # need to mock openAI to get answer from openAI
-    assert json_data["answer"] == "the question and answer to it"
+    when(openAIAskQuestion).ask().thenReturn(openAIAskQuestion.answer)
+    assert json_data["question"] == openAIAskQuestion.question
+    assert json_data["answer"] == openAIAskQuestion.answer
+    verify(openAIAskQuestion).ask()
