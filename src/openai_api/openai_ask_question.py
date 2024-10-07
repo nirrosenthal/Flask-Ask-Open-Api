876c9b01529e9c29e54268cbde2769afaab4e0a5
ngrvendeta <74234047+nirrosenthal@users.noreply.github.com>
2024-10-07 11:01:37 +0300

    create class for asking question usecase, with chat completions

---
+++ src/openai_api/openai_ask_question.py
@@ -0,0 +1,32 @@
+from src.openai_api.client import OpenAIClient
+
+class OpenAIAskQuestion(OpenAIClient):
+
+    def __init__(self, question: str):
+        super().__init__()
+        self._question = question
+        self._answer = ""
+
+    @property
+    def question(self)->str:
+        return self._question
+
+    @property
+    def answer(self)->str:
+        return self._answer
+    
+    def ask(self)->None:
+        # add an empty question clause
+        if self._question=="":
+            return ""
+        
+        self._openai_response = self._client.chat.completions.create(
+        model = "gpt-4o-mini",
+        messages = [
+            {"role":"system", "content": "You are an assistant who answers questions"},
+            {"role": "user", "content": self._question}
+        ]
+        )
+        self._answer = self._openai_response.choices[0].message.content
+        return self._answer
+    
