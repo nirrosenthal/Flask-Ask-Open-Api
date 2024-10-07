86e47c142f62fcfcad74510ae18417df4a7a06f0
ngrvendeta <74234047+nirrosenthal@users.noreply.github.com>
2024-10-07 21:06:51 +0300

    added class singelton for DB engine, with a session factory

---
+++ src/database/engine.py
@@ -0,0 +1,41 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+
+
+# Database connection parameters
+username = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+host = 'localhost'
+port = os.getenv("POSTGRES_PORT")
+database = os.getenv("POSTGRES_DB")
+
+
+class DatabaseEngineSingleton:
+    __instance = None
+
+    def __new__(cls):
+        if DatabaseEngineSingleton.__instance is None:
+            cls.__instance = \
+                super(DatabaseEngineSingleton,cls).__new__(cls)
+            cls.__instance._connection_string = "postgresql://username:password@localhost:5432/your_database"
+            cls.__instance._engine = create_engine(cls.__instance.connection_string)   
+            cls.__instance._session_factory = sessionmaker(bind=cls.__instance._engine)
+        return cls.__instance
+    
+    @property
+    def engine(cls):
+        return cls.__instance._engine
+
+    @property
+    def connection_string(cls):
+        return cls.__instance._connection_string
+  
+
+    def session(cls):
+        return cls.__instance._session_factory()
+    
+if __name__ == "__main__":
+    db = DatabaseEngineSingleton()
+    with db.session() as Session:
+        print("Session creation successful")
