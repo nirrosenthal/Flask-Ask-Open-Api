5e667372b5c73268ac6bda7fc35b0fc4b69b3f6c
ngrvendeta <74234047+nirrosenthal@users.noreply.github.com>
2024-10-07 21:23:38 +0300

    fixes to connection_url

--- src/database/engine.py
+++ src/database/engine.py
@@ -9,7 +9,7 @@ password = os.getenv("POSTGRES_PASSWORD")
 host = 'localhost'
 port = os.getenv("POSTGRES_PORT")
 database = os.getenv("POSTGRES_DB")
-
+connection_url = f"postgresql://{username}:{password}@{host}:{port}/{database}"
 
 class DatabaseEngineSingleton:
     __instance = None
@@ -18,7 +18,7 @@ class DatabaseEngineSingleton:
         if DatabaseEngineSingleton.__instance is None:
             cls.__instance = \
                 super(DatabaseEngineSingleton,cls).__new__(cls)
-            cls.__instance._connection_string = "postgresql://username:password@localhost:5432/your_database"
+            cls.__instance._connection_url = connection_url
             cls.__instance._engine = create_engine(cls.__instance.connection_string)   
             cls.__instance._session_factory = sessionmaker(bind=cls.__instance._engine)
         return cls.__instance
@@ -28,8 +28,8 @@ class DatabaseEngineSingleton:
         return cls.__instance._engine
 
     @property
-    def connection_string(cls):
-        return cls.__instance._connection_string
+    def connection_url(cls)->str:
+        return cls.__instance.connection_url
   
 
     def session(cls):
